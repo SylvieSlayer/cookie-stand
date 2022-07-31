567d39231d9d2ac5e59e1341ee52d593c81ad7ab
SYLVIE FISHER <iwalkedwithasylvie@gmail.com>
2022-07-30 17:45:49 -0700

    fixed js errors

--- app.js
+++ app.js
@@ -1,103 +1,34 @@
 'use strict'
 
-//creating object literal 
-
-function custPerHour(min, max) {
-  return Math.random;
-}
-
-custPerHour.store = Math.random
-
-//Seattle 
-let Seattle = {
-  
-  minimum: 23,
-  maximum: 65,
-  aveCookies: 6.3,
-
-  randomCustNumber: function (){
-    return getRandomNumber (this.minimum, this.maximum);
-  }
-
-};
-
 //random number generator 
 function getRandomNumber(min, max) {
  
   return Math.floor(Math.random() * (max - min + 1) + min );
 }
 
-console.log(Seattle.randomCustNumber());
-
-//Tokyo
-let Tokyo = {
-  
-  minimum: 3,
-  maximum: 24,
-  aveCookies: 1.2,
-
-  randomCustNumber: function (){
-    return getRandomNumber (this.minimum, this.maximum);
-  }
-
-};
-
-//Dubai
-let Dubai = {
-  
-  minimum: 11,
-  maximum: 38,
-  aveCookies: 3.7,
-
-  randomCustNumber: function (){
-    return getRandomNumber (this.minimum, this.maximum);
-  }
-
-};
 
 function getRandomNumber(min, max) {
  
   return Math.floor(Math.random() * (max - min + 1) + min );
 }
 
-console.log(Dubai.randomCustNumber());
 
-//Paris
-let Paris = {
-  
-  minimum: 20,
-  maximum: 38,
-  aveCookies: 2.3,
+function cities(location, min, max, average, ) {
 
-  randomCustNumber: function (){
-    return getRandomNumber (this.minimum, this.maximum);
-  }
+  this.location = location
+  this.min = min
+  this.max = max
+  this.average = average
 
-};
-
-function getRandomNumber(min, max) {
- 
-  return Math.floor(Math.random() * (max - min + 1) + min );
+  this.printLocation = function () {
+    console.log(this.location)
+  };
 }
 
-console.log(Paris.randomCustNumber());
+let Seattle = new cities ('Seattle', 23, 65, 6.3);
+let Tokyo = new cities ('Tokyo', 3, 24, 1.2);
+let Dubai = new cities ('Dubai', 11, 38, 3.7);
+let Paris = new cities ('Paris', 20, 38, 2.3)
+let Lima = new cities ('Lima', 2, 16, 4.6)
 
-//Lima
-let Lima = {
-  
-  minimum: 2,
-  maximum: 16,
-  aveCookies: 4.6,
-
-  randomCustNumber: function (){
-    return getRandomNumber (this.minimum, this.maximum);
-  }
-
-};
-
-function getRandomNumber(min, max) {
- 
-  return Math.floor(Math.random() * (max - min + 1) + min );
-}
-
-console.log(Lima.randomCustNumber());
+console.log(Seattle, Tokyo, Dubai, Paris, Lima)
